ac25402cf802de8d0b992bdc7d6b637525afed23
thaigithurb <23631761.thai@student.iuh.edu.vn>
2025-01-03 07:06:08 +0700

    project02

---
+++ assets/scripts/script.js
@@ -0,0 +1,15 @@
+AOS.init();
+
+var swiper = new Swiper(".swiper-blog", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
